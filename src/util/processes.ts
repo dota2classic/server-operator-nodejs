7bb34b7926d310a1c8080623e7f4317d3e7bf236
Yuri Stepin <enchantinggg4@gmail.com>
2024-11-17 22:00:33 +0300

    Fix wrong process identifying on dedicated server

--- src/util/processes.ts
+++ src/util/processes.ts
@@ -1,5 +1,5 @@
-import * as ps from "ps-node"
-import { CommandLineConfig } from "src/operator/command/launch-game-server.handler";
+import * as find from 'find-process';
+import { CommandLineConfig } from 'src/operator/command/launch-game-server.handler';
 
 export interface SrcdsProcess {
     port: number;
@@ -7,54 +7,62 @@ export interface SrcdsProcess {
     match: CommandLineConfig;
 }
 
+interface ProcessWrapper {
+    pid: number;
+    arguments: string[];
+}
+
+
+const checkProcess = (inf: ProcessWrapper): SrcdsProcess | null => {
+
+    const indexOfPort = inf.arguments.indexOf('-port');
+    const indexOfMatchdata = inf.arguments.indexOf('-match');
+    const port = parseInt(inf.arguments[indexOfPort + 1]);
+    const pid = inf.pid;
+
+    if (inf.arguments[0] === '<defunct>') {
+        // we need to kill this process
+        process.kill(pid);
+        console.warn('Killed orphaned srcds server on pid ' + pid);
+        return null;
+    }
+
+    try {
+        const matchInfo = JSON.parse(atob(inf.arguments[indexOfMatchdata + 1]));
+        return {
+            pid,
+            port,
+            match: matchInfo,
+        };
+    } catch (e) {
+        console.log(inf);
+        console.log('ERROR HERE!', e);
+        return null;
+    }
+
+};
+
 export async function getRunningSrcds(): Promise<SrcdsProcess[]> {
 
-    const regex = process.platform === 'win32' ? '.*srcds\\.exe.*' : '.*srcds_linux.*';
-    return new Promise((resolve, reject) => {
-        ps.lookup({
-            command: regex
-        }, (err, list) => {
-            if(err){
-                reject([]);
-                return;
-            }
-    
-            const res:SrcdsProcess[] =  list.map((inf) => {
+    // const regex = process.platform === 'win32' ? '.*srcds\\.exe.*' : '.*srcds_linux.*';
+    const regex = process.platform === 'win32' ? 'srcds.exe' : 'srcds_linux';
 
-                const indexOfPort = inf.arguments.indexOf('-port');
-                const indexOfMatchdata = inf.arguments.indexOf('-match')
-                const port = parseInt(inf.arguments[indexOfPort + 1]);
-                const pid = parseInt(inf.pid);
+    const result: SrcdsProcess[] = await find('name', regex, false)
+      .then(function(list) {
+          const r: (SrcdsProcess | null)[] = list.map(proc => {
+              const args = proc.cmd.split(/\s+/g);
 
-                if(inf.arguments[0] === '<defunct>'){
-                    // we need to kill this process
-                    process.kill(pid);
-                    console.warn('Killed orphaned srcds server on pid ' + pid);
-                    return null;
-                }
+              return checkProcess({
+                  pid: proc.pid,
+                  arguments: args,
+              });
+          })
 
-                try {
-                    const matchInfo = JSON.parse(atob(inf.arguments[indexOfMatchdata + 1]));
-                    return {
-                        pid,
-                        port,
-                        match: matchInfo
-                    }
-                }catch(e){
-                    console.log(inf)
-                    console.log("ERROR HERE!", e)
-                    return null;
-                }
 
-            }).filter(Boolean);
+          return r.filter(Boolean);
+      })
 
-            
-
-            // console.log(list);
-            resolve(res);
-        })
-    })
-
+    return result;
 
     // const cmd = (() => {
     //   switch (process.platform) {
@@ -64,15 +72,15 @@ export async function getRunningSrcds(): Promise<SrcdsProcess[]> {
     //     default: return false
     //   }
     // })()
-  
+
     // if( ! cmd ) {
     //   return false;
     // }
-  
+
     // return new Promise((resolve, reject) => {
     //   require('child_process').exec(cmd, (err: Error | null, stdout: string, stderr: string) => {
     //     if (err) reject(err);
-  
+
     //     console.log(stdout)
     //     resolve(stdout.toLowerCase().indexOf(processName.toLowerCase()) > -1)
     //   })
