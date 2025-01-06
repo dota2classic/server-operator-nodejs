6b0486be8e370d9a32507ab4ef8c64bb9e94744a
Yuri Stepin <enchantinggg4@gmail.com>
2025-01-06 03:13:59 +0300

    Fixes

--- src/metrics.service.ts
+++ src/metrics.service.ts
@@ -24,7 +24,14 @@ export class MetricsService {
   constructor(
     private readonly rconService: RconService,
     private readonly srcdsService: SrcdsService,
-  ) {}
+  ) {
+    this.collectServerMetrics({
+      host: `156.253.249.142`,
+      port: 27015,
+    } as any).then((some) => {
+      console.log(some);
+    });
+  }
 
   @Cron(CronExpression.EVERY_5_SECONDS)
   private async collectMetrics() {
---
+++ src/util/parseStatsResponse.spec.ts
@@ -0,0 +1,18 @@
+import { parseStatsResponse, SrcdsServerMetrics } from './parseStatsResponse';
+
+describe('parseStatsResponse', () => {
+  it('should parse running server', () => {
+    const some = parseStatsResponse(
+      `CPU   In    Out   Uptime  Users   FPS    Players\n31.01  2.00  4.00       3     0   59.94       9\nL 01/06/2025 - 00:03:30: rcon from \\"156.253.249.142:55454\\": command \\"stats\\"\n`,
+    );
+
+    expect(some).toMatchObject({
+      cpu: 31.01,
+      fps: 59.94,
+      uptime: 3,
+      in: 2.0,
+      out: 4.0,
+      users: 0,
+    } satisfies Partial<SrcdsServerMetrics>);
+  });
+});
