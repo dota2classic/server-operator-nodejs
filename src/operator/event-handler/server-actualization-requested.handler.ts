94e914c218efead35ae411744bdc963666cde625
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-09 02:39:32 +0300

    test

--- src/operator/event-handler/server-actualization-requested.handler.ts
+++ src/operator/event-handler/server-actualization-requested.handler.ts
@@ -3,11 +3,14 @@ import { ServerActualizationRequestedEvent } from 'src/gateway/events/gs/server-
 import { ServerStatusEvent } from 'src/gateway/events/gs/server-status.event';
 import { ConfigService } from '@nestjs/config';
 import { DockerService } from '../../docker/docker.service';
+import { Logger } from '@nestjs/common';
 
 @EventsHandler(ServerActualizationRequestedEvent)
 export class GameServerNotStartedHandler
   implements IEventHandler<ServerActualizationRequestedEvent>
 {
+  private logger = new Logger(GameServerNotStartedHandler.name);
+
   constructor(
     private readonly ebus: EventBus,
     private readonly config: ConfigService,
@@ -16,6 +19,9 @@ export class GameServerNotStartedHandler
 
   async handle(event: ServerActualizationRequestedEvent) {
     const host = event.url.split(':')[0];
+    this.logger.log(
+      `Actualization requested for server ${event.url}. Handling? ${this.config.get('srcds.host') === host}`,
+    );
     if (this.config.get('srcds.host') !== host) return;
 
     const running = await this.docker.getRunningGameServers();
