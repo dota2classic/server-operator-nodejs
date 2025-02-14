62ca2fc1545675db4f946066d4514e93097bbbc0
Yuri Stepin <enchantinggg4@gmail.com>
2025-02-14 16:45:42 +0300

    Use good ports first

--- src/util/getFreePort.ts
+++ src/util/getFreePort.ts
@@ -1,6 +1,31 @@
 import * as net from 'net';
 
+const isPortFree = async (port: number) => {
+  return new Promise<boolean>((res) => {
+    const server = net.createServer();
+    server.once('error', function (err) {
+      if (err.code === 'EADDRINUSE') {
+        // port is currently in use
+      }
+    });
+
+    server.once('listening', function () {
+      // close the server if listening doesn't fail
+      server.close();
+    });
+
+    server.listen(port);
+  });
+};
+
 export async function getFreePort(): Promise<number> {
+  for (let port = 27015; port < 29000; port += 10) {
+    if (await isPortFree(port)) {
+      return port;
+    }
+  }
+
+  console.war'Had to fallback to random free port't;")
   return new Promise<number>((res) => {
     const srv = net.createServer();
     srv.listen(0, () => {
